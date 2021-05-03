0308f02e8160cb9503002cdbe5e29b60e6426bee
unknown <ti_jonathan@hotmail.com>
2021-05-02 21:23:59 -0400

    Conversor de Moeda

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-
 import './App.css';
 import Conversor from "./components/Conversor"
 
 function App() {
   return (
     <div className="App">
-   <Conversor moedaA="RUS" moedaB="BRL"></Conversor>
-   <Conversor moedaA="RUS" moedaB="BRL"></Conversor>
-   <Conversor moedaA="RUS" moedaB="BRL"></Conversor>
-   <Conversor moedaA="RUS" moedaB="BRL"></Conversor>
+      <h1>Conversor de Moeda</h1>
+      <div className="linha">
+   <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+   <Conversor moedaA="BRL" moedaB="USD"></Conversor>
     </div>
+
+    <div className="linha">
+   <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+   <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+    </div>
+
+    <div className="linha">
+   <Conversor moedaA="OMR" moedaB="BRL"></Conversor>
+   <Conversor moedaA="BRL" moedaB="OMR"></Conversor>
+    </div>
+
+    </div>
+
   );
 }
 
